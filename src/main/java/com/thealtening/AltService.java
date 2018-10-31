0e1c710061f5c2a690164233d76b303345a37280
Trol <trol1337gmaing@gmail.com>
2018-10-31 14:14:18 -0400

    Fixed the API -> logging in works!

--- src/main/java/com/thealtening/AltService.java
+++ src/main/java/com/thealtening/AltService.java
@@ -28,7 +28,7 @@ public class AltService {
     private void reflectionFields(String authServer) throws NoSuchFieldException, IllegalAccessException {
         final HashMap<String, URL> userAuthenticationModifies = new HashMap();
         userAuthenticationModifies.put("ROUTE_AUTHENTICATE", constantURL("https://authserver." + authServer + ".com/authenticate"));
-        userAuthenticationModifies.put("ROUTE_INVALIDATE", constantURL("https://authserver" + authServer + "thealtening.com/invalidate"));
+        userAuthenticationModifies.put("ROUTE_INVALIDATE", constantURL("https://authserver" + authServer + "com/invalidate"));
         userAuthenticationModifies.put("ROUTE_REFRESH", constantURL("https://authserver." + authServer + ".com/refresh"));
         userAuthenticationModifies.put("ROUTE_VALIDATE", constantURL("https://authserver." + authServer + ".com/validate"));
         userAuthenticationModifies.put("ROUTE_SIGNOUT", constantURL("https://authserver." + authServer + ".com/signout"));
@@ -40,8 +40,10 @@ public class AltService {
                 e.printStackTrace();
             }
         });
-        userAuthentication.setStaticField("BASE_URL", "https://authserver." + authServer + ".com");
-        minecraftSession.setStaticField("BASE_URL", "https://sessionserver." + authServer + ".com");
+        userAuthentication.setStaticField("BASE_URL", "https://authserver." + authServer + ".com/");
+        minecraftSession.setStaticField("BASE_URL", "https://sessionserver." + authServer + ".com/session/minecraft/");
+        minecraftSession.setStaticField("JOIN_URL", constantURL("https://sessionserver." + authServer + ".com/session/minecraft/join"));
+        minecraftSession.setStaticField("CHECK_URL", constantURL("https://sessionserver." + authServer + ".com/session/minecraft/hasJoined"));
         minecraftSession.setStaticField("WHITELISTED_DOMAINS", new String[]{".minecraft.net", ".mojang.com", ".thealtening.com"});
     }
 
